1faf710e49073b749b91bd388df6e63a29d5e83f
Mateusz Modrzejewski <ma.modrzejewski@gmail.com>
2021-11-06 17:53:03 +0100

    feat: add and sort images

--- js/main.js
+++ js/main.js
@@ -4,70 +4,71 @@
     // Spinner
     var spinner = function () {
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+            if ($("#spinner").length > 0) {
+                $("#spinner").removeClass("show");
             }
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
-        time: 2000
+        time: 2000,
     });
 
-
     // Typed Initiate
-    if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
+    if ($(".typed-text-output").length == 1) {
+        var typed_strings = $(".typed-text").text();
+        var typed = new Typed(".typed-text-output", {
+            strings: typed_strings.split(", "),
             typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
-            loop: true
+            loop: true,
         });
     }
 
-
     // Smooth scrolling to section
-    $(".btn-scroll").on('click', function (event) {
+    $(".btn-scroll").on("click", function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 0
-            }, 1500, 'easeInOutExpo');
+
+            $("html, body").animate(
+                {
+                    scrollTop: $(this.hash).offset().top - 0,
+                },
+                1500,
+                "easeInOutExpo"
+            );
         }
     });
-    
-    
-    // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
 
+    // Skills
+    $(".skill").waypoint(
+        function () {
+            $(".progress .progress-bar").each(function () {
+                $(this).css("width", $(this).attr("aria-valuenow") + "%");
+            });
+        },
+        { offset: "80%" }
+    );
 
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+    var portfolioIsotope = $(".portfolio-container").isotope({
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+        filter: ".live",
     });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+    $("#portfolio-flters li").on("click", function () {
+        $("#portfolio-flters li").removeClass("active");
+        $(this).addClass("active");
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data("filter") });
     });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -75,21 +76,19 @@
         smartSpeed: 1500,
         dots: true,
         loop: true,
-        items: 1
+        items: 1,
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+            $(".back-to-top").fadeIn("slow");
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $(".back-to-top").fadeOut("slow");
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
     });
 })(jQuery);
-
